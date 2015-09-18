1127a127686f13eeba216e6cf17a0282ff4cde05
Yvonne <ypeng.tw@gmail.com>
2015-09-18 11:30:39 -0700

    Pass lastBottleLyrics test. It will make sure the last bottle lyrics is showing correctly

--- spec/specs.js
+++ spec/specs.js
@@ -4,3 +4,10 @@ describe("getLyrics", function() {
     expect(getLyrics(8)).to.equal(output);
   });
 });
+
+describe("lastBottleLyrics", function() {
+  it("return the last bottle lyrics with the inputted number", function() {
+    var output = "<p><font color='orange'>1</font> bottle of beer on the wall, <font color='orange'>1</font> bottle of beer. <br>Take one down and pass it around, no more bottles of beer on the wall.</p><p><font color='green'>No more bottles of beer on the wall, no more bottles of beer.<br> Go to the store and buy some more, <font color='orange'>" + 5 + "</font> bottles of beer on the wall.</font></p>";
+    expect(lastBottleLyrics(5)).to.equal(output);
+  });
+});
