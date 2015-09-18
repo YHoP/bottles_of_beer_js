53fc18a4022658580c863122b2b1b91ba87110f3
Yvonne <ypeng.tw@gmail.com>
2015-09-18 11:23:18 -0700

    Add lastBottleLyrics function. It will display the last bottle lyrics

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,26 @@
 var getLyrics = function (number){
-  var lyrics = "";
-  for (var i = number; i > 1 ; i--){
-    lyrics += "<p><font color='orange'>" + i + "</font> bottles of beer on the wall, <font color='red'>" + i + "</font> bottles of beer. <br> Take one down and pass it around, <font color='red'>" + (i - 1) + "</font> bottles of beer on the wall. </p>";
-  }
+  var lyrics = "<p><font color='orange'>" + number + "</font> bottles of beer on the wall, <font color='orange'>" + number + "</font> bottles of beer. <br>Take one down and pass it around, <font color='orange'>" + (number - 1) + "</font> bottles of beer on the wall.</p>";
+  return lyrics;
+};
+
+var lastBottleLyrics = function (number){
+  var lyrics = "<p><font color='orange'>1</font> bottle of beer on the wall, <font color='orange'>1</font> bottle of beer. <br>Take one down and pass it around, no more bottles of beer on the wall.</p><p><font color='green'>No more bottles of beer on the wall, no more bottles of beer.<br> Go to the store and buy some more, <font color='orange'>" + number + "</font> bottles of beer on the wall.</font></p>";
   return lyrics;
 };
 
 
 $(document).ready(function() {
   $("form#numberOfBottles").submit(function(event) {
+    $(".lyrics").empty();
     var number = $("input#number").val();
-    var lyrics = getLyrics(number);
 
-    $(".lyrics").append(lyrics);
-    $("#song").fadeIn();
+    for (var i = number; i > 0; i --){
+      if(i === 1){
+        $(".lyrics").append(lastBottleLyrics(number));
+      }else{
+        $(".lyrics").append(getLyrics(i));
+      }
+    }
 
     event.preventDefault();
   });
